c490954f2f98d4f8cca7f3bddcb59a9b9ff1c254
Julia Seidman <juliaseid@gmail.com>
2020-08-21 14:39:34 -0700

    dev environment set up

---
+++ build/precache-manifest.b0c3414fcf4ccbe84828e1720d639ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2268245b2540cdc74cf31505869bd13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a14e22d68e905eb1c799",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "610dd8a969911c2599eb",
+    "url": "/static/js/2.6e386246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e386246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14e22d68e905eb1c799",
+    "url": "/static/js/main.55df282c.chunk.js"
+  },
+  {
+    "revision": "97bebf632bc8018b3031",
+    "url": "/static/js/runtime-main.b01629fd.js"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../sass/styles.scss";
+// import "https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>This is a React app.</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
